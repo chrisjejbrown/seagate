4a2ecaf5d61a5c9cb69e2bb6bbe8e5ed0f6aa650
Tucker Elliott <telliott@adobe.com>
2024-06-17 10:15:04 -0400

    fix table links

--- tools/importer/import2.js
+++ tools/importer/import2.js
@@ -15,7 +15,6 @@ export default {
       const meta = WebImporter.Blocks.getMetadata(document);
       const authorName = document.querySelector('.author-card--author-name')?.textContent;
       const publishDate = document.querySelector('.author-card--date')?.textContent || '';
-      const tags = document.querySelectorAll('.author-card--tags a');
       const authorImage = document.querySelector('.author-card--author-image')?.src;
       meta.Author = authorName || '';
       if (publishDate === '') {
@@ -24,64 +23,8 @@ export default {
         // eslint-disable-next-line prefer-destructuring
         meta['Publication Date'] = new Date(publishDate).toISOString().split('T')[0];
       }
-      meta.Tags = Array.from(tags).map((tag) => tag.textContent).join(', ') || '';
-      const youtubeIframes = document.querySelectorAll('iframe[src*="youtube"]');
   
-      // Handle youtube videos
-      youtubeIframes.forEach((iframe) => {
-        // replace the iframe with the video URL
-        const videoUrl = iframe.src;
-        const video = document.createElement('a');
-        video.href = videoUrl;
-        video.textContent = videoUrl;
-        iframe.parentElement.replaceChild(video, iframe);
-      });
-  
-      // Handle CTAs
-      const ctas = document.querySelectorAll('.action-component .js-react-action-component');
-      ctas.forEach((cta) => {
-        const slingProps = JSON.parse(cta.getAttribute('data-sling-props'));
-        if (slingProps.ctaText === 'TRY SLING TV TODAY!') {
-          const ctaFragment = document.createElement('a');
-          ctaFragment.href = CTA_FRAGMENT_URL;
-          ctaFragment.textContent = CTA_FRAGMENT_URL;
-          cta.parentElement.replaceChild(ctaFragment, cta);
-        }
-      });
-      // Handle other CTAs
-      const otherCtas = document.querySelectorAll('a.cta-button');
-      otherCtas.forEach((cta) => {
-        // replace with CTA_FRAGMENT_URL
-        const ctaFragment = document.createElement('a');
-        ctaFragment.href = CTA_FRAGMENT_URL;
-        ctaFragment.textContent = CTA_FRAGMENT_URL;
-        cta.parentElement.replaceChild(ctaFragment, cta);
-      });
-  
-      // Handle tables
-      const tables = document.querySelectorAll('table');
-      tables.forEach((table) => {
-        const cells = [['Table'],
-          [table.outerHTML],
-        ];
-        const newTable = WebImporter.DOMUtils.createTable(cells, document);
-        table.parentElement.replaceChild(newTable, table);
-      });
-  
-      // Handle Carousels
-      const carousels = document.querySelectorAll('.image-carousel');
-      carousels.forEach((carousel) => {
-        const images = carousel.querySelectorAll('img');
-        if (images.length > 1) {
-          const cells = [['Carousel']];
-          images.forEach((img) => {
-            cells.push([img.outerHTML]);
-          });
-          const newTable = WebImporter.DOMUtils.createTable(cells, document);
-          carousel.parentElement.replaceChild(newTable, carousel);
-        }
-      });
-  
+
       // Handle category pages
       const isCategoryPage = document.querySelector('.homepage-wrapper .blog-homepage--outer');
       if (isCategoryPage) {
@@ -143,23 +86,7 @@ export default {
       // Add metadata block to the document
       const block = WebImporter.Blocks.getMetadataBlock(document, meta);
   
-      // Handle anchor links or odd links
-      if (main.querySelector('a[href^="#"]')) {
-        const u = new URL(url);
-        const links = main.querySelectorAll('a[href^="#"]');
-        for (let i = 0; i < links.length; i += 1) {
-          const a = links[i];
-          a.href = `${u.pathname}${a.getAttribute('href')}`;
-        }
-      }
-  
-      // Handle relative links
-      const relativeLinks = main.querySelectorAll('a[href^="/"]');
-      for (let i = 0; i < relativeLinks.length; i += 1) {
-        const a = relativeLinks[i];
-        a.href = `${HOSTNAME}${a.getAttribute('href')}`;
-      }
-  
+
       // // append the block to the main element
       main.append(block);
   
